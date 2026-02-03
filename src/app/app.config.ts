cd543baf286f83608f35cdfc54ef79820a55287e
Juan Jose Pineda Sanchez <124926749+JuanSanchez1193@users.noreply.github.com>
2026-02-03 13:29:23 -0500

    configure basic firebase standalone config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,32 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+
 import { routes } from './app.routes';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    // Manejo global de errores del navegador
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+
+    // Router de Angular
+    provideRouter(routes),
+
+    // Firebase Core
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+
+    // Firebase Authentication
+    provideAuth(() => getAuth()),
+
+    //Firestore Database
+    provideFirestore(() => getFirestore()),
+
+    //Firebase Storage
+    provideStorage(() => getStorage()),
   ]
 };
